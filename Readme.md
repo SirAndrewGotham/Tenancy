e0e9c60553950064d2d9dbbe0c72baef1a836ba0
Andrew Gotham <andrewgotham@hotmail.com>
2025-03-04 17:31:52 +0300

    initial commit with tenants table scaffolding

---
+++ Readme.md
@@ -0,0 +1,3 @@
+## Tenancy
+
+Multitenancy project built with Laravel 12, Livewire, Alpine and Tailwind using single database.
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Admin',
+            'slug' => 'admin',
+            'email' => 'admin@admin.com',
+        ]);
+
+        User::factory()
+            ->count(10)
+            ->create(
+                ['tenant_id' => 1]
+            );
+
+        User::factory()
+            ->count(10)
+            ->create();
+    }
+}
